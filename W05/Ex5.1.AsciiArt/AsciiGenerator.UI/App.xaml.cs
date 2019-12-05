902c7d6243e35097a53edfc439dfd3a93828a264
omd <ms@odddots.ch>
2019-12-05 12:13:32 +0100

    Musterlösung für Übung 5 (Ascii) ergänzt

--- W05/Ex5.1.AsciiArt/AsciiGenerator.UI/App.xaml.cs
+++ W05/Ex5.1.AsciiArt/AsciiGenerator.UI/App.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System.Windows;
+using AsciiArtGenerator.ViewModels;
 using AsciiGenerator.UI.Util;
+using AsciiGenerator.UI.Views;
 
 namespace AsciiGenerator.UI
 {
@@ -11,17 +13,31 @@ namespace AsciiGenerator.UI
 
         public WpfPlatformSupport PlatformSupport { get; set; } = new WpfPlatformSupport();
 
+        // hier wurde eine Klassen-Property angelegt, man hätte auch einfach eine
+        // lokale Variable in der OnStartup-Methode verwenden können (hätte dann
+        // aber zu einem späteren Zeitpunkt im App-Lifecycle keinen direkten
+        // Zugriff mehr darauf)
+        public AsciiGeneratorVm AppVm { get; set; }
+
+
         protected override void OnStartup(StartupEventArgs e)
         {
             base.OnStartup(e);
+            AppVm = new AsciiGeneratorVm();
+            // hier fügen wir per Dependency Injection die konkreten Implementierungen
+            // des ChooseFile-Dialogs (bereits vorimplementiert in Projektvorlage)
+            // und einer Message Box zur Anzeige eines Fehler ein
+            AppVm.OnChooseFile = PlatformSupport.ChooseFile;
+            AppVm.OnShowError = PlatformSupport.ShowError;
+            // Das ViewModel bietet hier als Erweiterungspunkt aus Bequemlichkeit einzelne Methoden an
+            // In der Praxis werden es eher ganze Interfaces sein und wir würden pro Plattform eine
+            // Klasse mit einer konkreten Implementierung des Interfaces erstellen, hier instanziieren
+            // und dann zuweisen
 
-            // hier den Glue Code ergänzen (DataContext von MainWindow,
-            // sowie ViewModels etc.):
-            // ...          
+            MainWindow = new MainWindow();
+            MainWindow.DataContext = AppVm;
+            MainWindow.Show();
 
-            // ... und nicht vergessen, im App.xaml die Property
-            // StartupUri zu entfernen, falls das MainWindow hier
-            // erzeugt und angezeigt wird (wie z.B. in Ü4).
         }
     }
 }
