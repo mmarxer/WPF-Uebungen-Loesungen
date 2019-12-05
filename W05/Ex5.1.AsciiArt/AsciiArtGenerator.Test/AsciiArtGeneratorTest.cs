0e5c68e0b96cf029f463983e8bba9ab2fbe524de
omd <ms@odddots.ch>
2019-12-05 12:12:08 +0100

    Ausgangslage Aufgabe 1 (Ascii) für Übung 5 ergänzt

---
+++ W05/Ex5.1.AsciiArt/AsciiArtGenerator.Test/AsciiArtGeneratorTest.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AsciiArtGenerator.Test
+{
+    [TestClass]
+    public class AsciiArtGeneratorTest
+    {
+        private static string StartupPath => System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location);
+        private static string MediaBasePath => System.IO.Path.Combine(StartupPath, "media");
+
+        private static string GetMediaPath(string folder)
+        {
+            return System.IO.Path.Combine(MediaBasePath, folder);
+        }
+        
+        private IEnumerable<string> GetFilesInFolder(string folder)
+        {
+            return System.IO.Directory.GetFiles(GetMediaPath(folder));
+        } 
+
+        private void PrintAllImages(IEnumerable<string> filenames, int width = 160)
+        {
+            var sw = new Stopwatch();
+            sw.Start();
+            foreach (var f in filenames)
+            {
+                Debug.Write($"Generating ASCII art for file {f}...");
+                var sw2 = new Stopwatch();
+                sw2.Start();
+                var bm = (Bitmap)Image.FromFile(f);
+                var generator = new Generator();
+                var ascii = generator.GenerateFrom(bm, width);
+                sw2.Stop();
+                Debug.WriteLine($"Done in {sw.Elapsed.TotalSeconds}s:");
+
+                Debug.WriteLine(ascii);
+                Debug.WriteLine(new string('-', width));
+            }
+            sw.Stop();
+            Debug.WriteLine($"Generated ASCII art collection in {sw.Elapsed.TotalSeconds}s");
+            Debug.WriteLine(new string('=', width));
+        }
+
+        [TestMethod]
+        public void TestHeroPix()
+        {
+            PrintAllImages(GetFilesInFolder("heroes"));
+        }
+
+        [TestMethod]
+        public void TestNaturePix()
+        {
+            PrintAllImages(GetFilesInFolder("nature"));
+        }
+
+        [TestMethod]
+        public void TestThingsPix()
+        {
+            PrintAllImages(GetFilesInFolder("things"));
+        }
+    }
+}
---
+++ W05/Ex5.1.AsciiArt/AsciiGenerator.UI/App.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System.Windows;
+using AsciiGenerator.UI.Util;
+
+namespace AsciiGenerator.UI
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+
+        public WpfPlatformSupport PlatformSupport { get; set; } = new WpfPlatformSupport();
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            // hier den Glue Code ergänzen (DataContext von MainWindow,
+            // sowie ViewModels etc.):
+            // ...          
+
+            // ... und nicht vergessen, im App.xaml die Property
+            // StartupUri zu entfernen, falls das MainWindow hier
+            // erzeugt und angezeigt wird (wie z.B. in Ü4).
+        }
+    }
+}
