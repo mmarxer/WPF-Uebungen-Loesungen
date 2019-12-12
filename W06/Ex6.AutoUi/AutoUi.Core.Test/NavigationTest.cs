fb94b310582bbd0d0a7f766927d890eeb926d508
omd <ms@odddots.ch>
2019-12-12 17:10:31 +0100

    Ü6 (AutoUi) - Musterlösung zu Aufgabe 4

---
+++ W06/Ex6.AutoUi/AutoUi.Core.Test/NavigationTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoUi.Core.ViewModels;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AutoUi.Core.Test
+{
+    [TestClass]
+    public class NavigationTest
+    {
+        [TestMethod]
+        public void TopWindowIsCorrectlySet()
+        {
+            // 1. Arrange
+            var navService = new TestMockNavigationService();
+            var vm = new AutoListVm();
+
+            // 2. Act
+            navService.Show<AutoListVm>(vm);
+            var windowOnTopAfter = navService.ViewStack.Last();
+
+            // 3. Assert
+            Assert.AreEqual("AutoListVm", windowOnTopAfter);
+        }
+    }
+}
---
+++ W06/Ex6.AutoUi/AutoUi.Core.Test/TestMockNavigationService.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using AutoUi.Core.Services;
+
+namespace AutoUi.Core.Test
+{
+    public class TestMockNavigationService : INavigationService
+    {
+        public Stack<string> ViewStack { get; } = new Stack<string>();
+
+        public void Show<T>(T vm)
+        {
+            ViewStack.Push(typeof(T).Name);
+        }
+    }
+}
