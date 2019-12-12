730fbf59f4b4cfba14bc28d692527d1e5115a7e3
omd <ms@odddots.ch>
2019-12-12 11:12:57 +0100

    Ü6 (AsciiArt) - Musterlösung zu Aufgabe 3

--- W06/Ex6.AsciiArt/AsciiGenerator.UI/Views/MainWindow.xaml.cs
+++ W06/Ex6.AsciiArt/AsciiGenerator.UI/Views/MainWindow.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System.Windows;
+﻿using System;
+using System.Windows;
+using System.Windows.Input;
+using AsciiArtGenerator.ViewModels;
 
 namespace AsciiGenerator.UI.Views
 {
@@ -11,6 +14,35 @@ namespace AsciiGenerator.UI.Views
         {
             InitializeComponent();
 
+        }
+
+        private void UIElement_OnMouseWheel(object sender, MouseWheelEventArgs e)
+        {
+            // für Event Handling müssen wir manchmal die MVVM-Idee
+            // verletzen und doch auch aus dem Code Behind auf ein
+            // ViewModel zugreifen (enge Kopplung). Wenn das die 
+            // Ausnahme bleibt, ist das ok...
+
+            // Um Zugriff auf das ViewModel zu erhalten, benutzen wir
+            // einfach den DataContext und casten in entsprechend in
+            // unseren ViewModel-Datentyp
+            var vm = DataContext as AsciiGeneratorVm;
+
+            if (e.Delta < 0)
+            {
+                if (vm.FontSize > 6) // Lower Bounds Check
+                    vm.FontSize -= 1;
+            }
+            else
+            {
+                if (vm.FontSize < 120) // Upper Bounds Check
+                    vm.FontSize += 1;
+            }
+
+            // Um zu verhindern, dass die Standard-Aktion der Textbox
+            // ausgeführt wird (= Scrollen in der TextBox bei übergrossem
+            // Inhalt), brechen wir das weitere Event-Handling ab:
+            e.Handled = true;
         }
     }
 }
